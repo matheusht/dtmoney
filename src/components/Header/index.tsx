167524601cfd199b00baa229702981a5859ea7c8
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-07-05 21:39:51 -0300

    feat: 🎸 1st commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logoImg from '../../assets/logo.svg';
+import Modal from 'react-modal'
+import { useState } from 'react';
+import { Container, Content } from './styles';
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
+    return (
+        <Container>
+            <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+            Nova Transação
+        </button>
+        </Content>
+        </Container>
+    );
+}
